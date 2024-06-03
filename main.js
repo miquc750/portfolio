1ec1e0e7f12bb5dd1dbbe650abd5660b2c907334
Cristina Miquel Garrido <miquc750@newschool.edu>
2024-06-03 12:43:32 +0200

    adding projects

--- main.js
+++ main.js
@@ -16,3 +16,50 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const projectsContainer = document.querySelector('.projects');
+
+            // Load "case-study"
+            data['case-study'].forEach(project => {
+                const projectElement = document.createElement('div');
+                projectElement.classList.add('project');
+
+                projectElement.innerHTML = `
+                    <a href="${url}" class="project-card">
+                        <img src="${project.cover__img}" alt="${project.title}">
+                        <section class="project-description">
+                            <h2>${project.title}</h2>
+                            <p>${project.briefing}</p>
+                            <time datetime="${project.date}">${project.date}</time>
+                        </section>
+                    </a>
+                `;
+
+                projectsContainer.appendChild(projectElement);
+            });
+
+            // Cargar "project"
+            data['project'].forEach(project => {
+                const projectElement = document.createElement('div');
+                projectElement.classList.add('project');
+
+                projectElement.innerHTML = `
+                    <a href="${url}" class="project-card">
+                        <img src="${project.cover__img}" alt="${project.title}">
+                        <section class="project-description">
+                            <h2>${project.title}</h2>
+                            <p>${project.briefing}</p>
+                            <time datetime="${project.date}">${project.date}</time>
+                        </section>
+                    </a>
+                `;
+
+                projectsContainer.appendChild(projectElement);
+            });
+        })
+        .catch(error => console.error('Error loading the projects:', error));
+});
