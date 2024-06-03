2212c98dea3a6f00c099cdb0f36c909e645fdd24
Cristina Miquel Garrido <miquc750@newschool.edu>
2024-06-03 13:07:54 +0200

    solving js

--- main.js
+++ main.js
@@ -22,42 +22,55 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             const projectsContainer = document.querySelector('.projects');
+            const caseStudiesContainer = document.querySelector('.case__study'); // Asegúrate de que este selector esté correctamente asignado
 
             // Load "case-study"
             data['case-study'].forEach(project => {
                 const projectElement = document.createElement('div');
-                projectElement.classList.add('project');
-
+                projectElement.classList.add('case__study');
+            
                 projectElement.innerHTML = `
-                    <a href="${url}" class="project-card">
+                    <a href="${project.url}">
+                    <div class="project-card">
                         <img src="${project.cover__img}" alt="${project.title}">
                         <section class="project-description">
                             <h2>${project.title}</h2>
-                            <p>${project.briefing}</p>
                             <time datetime="${project.date}">${project.date}</time>
+                            <p>${project.briefing}</p>
+                            <section class="pills">
+                                <div>${project.tag1}</div>
+                                <div>${project.tag2}</div>
+                            </section>
                         </section>
+                    </div>
                     </a>
                 `;
-
-                projectsContainer.appendChild(projectElement);
+            
+                caseStudiesContainer.appendChild(projectElement); // Cambiado de projectsContainer a caseStudiesContainer
             });
-
-            // Cargar "project"
+            
+            // Load "project"
             data['project'].forEach(project => {
                 const projectElement = document.createElement('div');
                 projectElement.classList.add('project');
-
+            
                 projectElement.innerHTML = `
-                    <a href="${url}" class="project-card">
+                    <a href="${project.url}">
+                    <div class="project-card">
                         <img src="${project.cover__img}" alt="${project.title}">
                         <section class="project-description">
                             <h2>${project.title}</h2>
-                            <p>${project.briefing}</p>
                             <time datetime="${project.date}">${project.date}</time>
+                            <p>${project.briefing}</p>
+                            <section class="pills">
+                                <div>${project.tag1}</div>
+                                <div>${project.tag2}</div>
+                            </section>
                         </section>
+                    </div>
                     </a>
                 `;
-
+            
                 projectsContainer.appendChild(projectElement);
             });
         })
