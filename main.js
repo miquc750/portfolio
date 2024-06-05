8a27b3692d89d4b079f89e9e013b70ff825590f3
Cristina Miquel Garrido <miquc750@newschool.edu>
2024-06-05 10:47:38 +0200

    Solve nav formatting

--- main.js
+++ main.js
@@ -1,21 +1,97 @@
-//LOADING OVERLAY
-
 document.addEventListener("DOMContentLoaded", () => {
+    const overlay = document.getElementById('loadingOverlay');
     setTimeout(() => {
-        const overlay = document.getElementById('loadingOverlay');
         overlay.style.opacity = '0';
         overlay.style.visibility = 'hidden';
     }, 2000);
+
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const projectsContainer = document.querySelector('.projects');
+            const caseStudiesContainer = document.querySelector('.case__study');
+            addProjects(data['case__study'], caseStudiesContainer);
+            addProjects(data['project'], projectsContainer);
+            initIntersectionObserver();
+        })
+        .catch(error => console.error('Error loading the projects:', error));
+
+    updateActiveNav();
 });
 
+// ADD PROJECTS TO DOM
+function addProjects(projects, container) {
+    projects.forEach(project => {
+        const projectElement = document.createElement('div');
+        projectElement.classList.add('project', 'project-card');
 
-//NAV ACTIVE
-let sections = document.querySelectorAll('main > section');
-let navLinks = document.querySelectorAll('header ul li');
+        projectElement.innerHTML = `
+            <a href="${project.url}">
+                <div class="project-card">
+                    <img src="${project.cover__img}" alt="${project.title}">
+                    <section class="project-description">
+                        <h2>${project.title}</h2>
+                        <time datetime="${project.date}">${project.date}</time>
+                        <p>${project.briefing}</p>
+                        <section class="pills">
+                            <div>${project.tag1}</div>
+                            <div>${project.tag2}</div>
+                        </section>
+                    </div>
+                </a>
+            `;
+        container.appendChild(projectElement);
+    });
+}
 
+// INITIALIZE INTERSECTION OBSERVER
+function initIntersectionObserver() {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    document.querySelectorAll('.project-card').forEach(card => {
+        observer.observe(card);
+    });
+}
+
+// ACCORDION
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var indicator = this.querySelector('.accordion-indicator');
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+            indicator.src = 'assets/plus.png';
+            indicator.alt = 'Expand';
+        } else {
+            panel.style.display = "block";
+            indicator.src = 'assets/minus.png';
+            indicator.alt = 'Collapse';
+        }
+    });
+}
+
+//UNIFYING SCROLL EVENTS
 window.onscroll = () => {
-    let scrollPosition = window.scrollY + window.innerHeight * 0.1;
+    updateActiveNav();
+    handleNavColor();
+};
 
+// ACTIVE NAV
+function updateActiveNav() {
+    let scrollPosition = window.scrollY + window.innerHeight * (-0.1);
+    let sections = document.querySelectorAll('main > section');
+    let navLinks = document.querySelectorAll('header ul li');
     let foundActive = false;
 
     sections.forEach(sec => {
@@ -44,109 +120,26 @@ window.onscroll = () => {
             }
         });
     }
-};
-
-window.onload = () => {
-    updateActiveNav();
-};
-
-function updateActiveNav() {
-    let scrollPosition = window.scrollY + window.innerHeight * 0.05;
-
-    sections.forEach(sec => {
-        let rect = sec.getBoundingClientRect();
-        let offset = window.scrollY + rect.top - 150;
-        let height = rect.height;
-
-        if (scrollPosition >= offset && scrollPosition < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                let targetLink = document.querySelector(`header ul li a[href="#${sec.id}"]`);
-                if (targetLink) {
-                    targetLink.parentElement.classList.add('active');
-                }
-            });
-        }
-    });
 }
 
+// DARK-LIGHT MODE NAV
+function handleNavColor() {
+    const sectionsDark = document.querySelectorAll('.dark-section');
+    let isDark = false;
 
-//ACCORDION
+    sectionsDark.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 8 && rect.bottom >= window.innerHeight / 8) {
+            isDark = true;
+        }
+    });
 
-var acc = document.getElementsByClassName("accordion");
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        var indicator = this.querySelector('.accordion-indicator');
-
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-            indicator.src = 'assets/plus.png';
-            indicator.alt = 'Expand';
+    const navElements = document.querySelectorAll('.nav-top, .nav-bottom');
+    navElements.forEach(element => {
+        if (isDark) {
+            element.classList.add('light-mode');
         } else {
-            panel.style.display = "block";
-            indicator.src = 'assets/minus.png';
-            indicator.alt = 'Collapse';
+            element.classList.remove('light-mode');
         }
     });
 }
-
-
-//PROJECT CARDS
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        const projectsContainer = document.querySelector('.projects');
-        const caseStudiesContainer = document.querySelector('.case__study');
-
-        function addProjects(projects, container) {
-            projects.forEach(project => {
-                const projectElement = document.createElement('div');
-                projectElement.classList.add('project', 'project-card');
-
-                projectElement.innerHTML = `
-                    <a href="${project.url}">
-                    <div class="project-card">
-                        <img src="${project.cover__img}" alt="${project.title}">
-                        <section class="project-description">
-                            <h2>${project.title}</h2>
-                            <time datetime="${project.date}">${project.date}</time>
-                            <p>${project.briefing}</p>
-                            <section class="pills">
-                                <div>${project.tag1}</div>
-                                <div>${project.tag2}</div>
-                            </section>
-                        </section>
-                    </div>
-                    </a>
-                `;
-
-                container.appendChild(projectElement);
-            });
-        }
-
-        addProjects(data['case__study'], caseStudiesContainer);
-        addProjects(data['project'], projectsContainer);
-
-        let observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('visible');
-                    observer.unobserve(entry.target);
-                }
-            });
-        }, {
-            threshold: 0.5
-        });
-
-        document.querySelectorAll('.project-card').forEach(card => {
-            observer.observe(card);
-        });
-    })
-    .catch(error => console.error('Error loading the projects:', error));
-});
