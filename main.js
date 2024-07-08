fc414ad5d89463829af85f968164b09a8d50b407
Cristina Miquel Garrido <miquc750@newschool.edu>
2024-07-08 19:42:45 +0200

    working on resolutions

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 //MOBILE MENU
 const menuToggle = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.mobile-menu');
+
 menuToggle.addEventListener('click', function() {
     mobileMenu.classList.toggle('open');
 });
